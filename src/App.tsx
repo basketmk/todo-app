216485fe2367631b65a05bb184342a4d7c966607
basketmk <basketboy-mk@yahoo.ne.jp>
2026-01-14 15:37:46 +0900

    コンポーネントの配置修正

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,33 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const Todo = (props) => {
+  return (
+    <ul className="flex m-4">
+      <li className="flex justify-between w-full items-start gap-3 ">
+        <label className="flex gap-3 min-w-0 flex-1">
+          <input type="checkbox" />
+          <span className="break-all">{props.title}</span>
+        </label>
+        <button className="border bg-gray-200 cursor-pointer flex-none w-6 h-6 flex items-center justify-center">
+          ×
+        </button>
+      </li>
+    </ul>
+  );
+};
+
+const todos = [
+  { title: "aaa", isCompleted: false },
+  { title: "bbb", isCompleted: false },
+  { title: "ccc", isCompleted: false },
+];
+
+const todoList = todos.map((todo) => {
+  return <Todo title={todo.title} isCompleted={todo.isCompleted} />;
+});
+
 function App() {
-  const Todo = (props) => {
-    return (
-      <ul className="flex m-4">
-        <li className="flex justify-between w-full items-start gap-3 ">
-          <label className="flex gap-3 flex-1 min-w-0 flex-1">
-            <input type="checkbox" />
-            <span className="break-all">{props.title}</span>
-          </label>
-          <button className="border bg-gray-200 cursor-pointer flex-none w-6 h-6 flex items-center justify-center">
-            ×
-          </button>
-        </li>
-      </ul>
-    );
-  };
-
-  const todos = [
-    { title: "aaa", isCompleted: false },
-    { title: "bbb", isCompleted: false },
-    { title: "ccc", isCompleted: false },
-  ];
-
-  const todoList = todos.map((todo) => {
-    return <Todo title={todo.title} isCompleted={todo.isCompleted} />;
-  });
-
   return (
     <div className="p-5 max-w-xl mx-auto">
       <div className="flex items-center justify-between gap-4 border-b-1 pb-3">
